5c8dca94d776dbc8d462b935c796f8a8edd7c076
juyeon <wjg365@naver.com>
2020-01-22 09:49:32 +0900

    Back

--- back-sk/src/main/java/com/web/curation/controller/account/AccountController.java
+++ back-sk/src/main/java/com/web/curation/controller/account/AccountController.java
@@ -1,26 +1,33 @@
 package com.web.curation.controller.account;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.validation.Valid;
 
- import com.web.curation.dao.user.UserDao;
-import com.web.curation.model.BasicResponse;
-import com.web.curation.model.user.SignupRequest;
- import com.web.curation.model.user.User;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.web.curation.dao.user.UserDao;
+import com.web.curation.model.BasicResponse;
+import com.web.curation.model.user.SignupRequest;
+import com.web.curation.naver.login.NaverLogin;
+
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.ApiOperation;
 
 
 @ApiResponses(value = {
@@ -35,6 +42,84 @@ public class AccountController{
     // 디비 셋팅 후 주석을 푸세요.
     @Autowired
     UserDao userDao;
+    
+    NaverLogin naver;
+    
+    
+    @GetMapping("/tmp")
+    @ApiOperation(value = "로그인")
+    public Object test2(
+            @RequestParam(value = "code") String code,
+            @RequestParam(value = "state") String state
+    ) throws Exception {
+        System.out.println("##########socail login");
+    	String clientId = "yW3gT9TqzIgQqklEfEBF";//애플리케이션 클라이언트 아이디값";
+        String naverClientSecret = "whK5eQJhow";
+
+        String apiURL;
+        apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
+        apiURL += "client_id=" + clientId;
+		apiURL += "&client_secret=" + naverClientSecret;
+        apiURL += "&code=" + code;
+        apiURL += "&state=" + state;
+        String access_token = "";
+        String refresh_token = "";
+        
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            System.out.println(con);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer res = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                res.append(inputLine);
+            }
+            br.close();
+            if(responseCode==200) { // 성공적으로 토큰을 가져온다면
+//            	System.out.println(res);
+                int id;
+                String nickName, email, tmp;
+                JsonParser parser = new JsonParser();
+                JsonElement accessElement = parser.parse(res.toString());
+                access_token = accessElement.getAsJsonObject().get("access_token").getAsString();
+
+                tmp = naver.getUserInfo(access_token);
+                JsonElement userInfoElement = parser.parse(tmp);
+                System.out.println("****************");
+                System.out.println(userInfoElement);
+                
+                id = userInfoElement.getAsJsonObject().get("response").getAsJsonObject().get("id").getAsInt();
+                nickName = userInfoElement.getAsJsonObject().get("response").getAsJsonObject().get("nickname").getAsString();
+                email = userInfoElement.getAsJsonObject().get("response").getAsJsonObject().get("email").getAsString();
+
+                access_token = naver.createJWTToken(id, nickName, email);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        JSONObject dummyUser = new JSONObject();
+
+        dummyUser.put("uid","test_uid");
+        dummyUser.put("email","test@test.com");
+        dummyUser.put("nickname","test_nickname");
+        
+        System.out.println(dummyUser);
+        final BasicResponse result = new BasicResponse();
+        result.status = true;
+        result.data = "success";
+        result.object = dummyUser.toMap();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+//        return "redirect:http://localhost:8080/agreement?token=" + access_token;
+    }
 
     @PostMapping("/account/login")
     @ApiOperation(value = "로그인")
