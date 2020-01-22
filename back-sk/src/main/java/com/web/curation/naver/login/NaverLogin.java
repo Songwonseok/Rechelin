0a391cb07769321fb404a0ab8ff0d72a90cefb4e
juyeon <wjg365@naver.com>
2020-01-22 11:37:41 +0900

    불피요한 코드 제거 , 아직 DB 연동 안되어있음 S02P12A404-44

--- back-sk/src/main/java/com/web/curation/naver/login/NaverLogin.java
+++ back-sk/src/main/java/com/web/curation/naver/login/NaverLogin.java
@@ -40,29 +40,5 @@ public class NaverLogin {
             return "Err";
         }
     }
-	public String createJWTToken(int id, String nickname, String email) {
-        String token = null;
-        DecodedJWT jwt = null;
-
-//        try {
-//            Long EXPIRATION_TIME = 1000L * 60L * 10L;
-//            Date issuedAt = new Date();
-//            Date notBefore = new Date(issuedAt.getTime());
-//            Date expiresAt = new Date(issuedAt.getTime() + EXPIRATION_TIME);
-//
-//            Algorithm algorithm = Algorithm.HMAC256(tokenSecret);
-//            token = JWT.create()
-//                    .withIssuer("auth0")
-//                    .withSubject(nickname)
-//                    .withAudience("1ilsang")
-//                    .withClaim("id", id)
-//                    .withClaim("email", email)
-//                    .withNotBefore(notBefore)
-//                    .withExpiresAt(expiresAt)
-//                    .sign(algorithm);
-//        } catch (Exception e) {
-//            System.err.println("err: " + e);
-//        }
-        return token;
-    }
+	
 }
