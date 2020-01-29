cab2f5d3f918eb27db9e8cb432bab3d4d621a395
ichhjkim <nillcafri123@gmail.com>
2020-01-29 15:23:54 +0900

    S02P12A404-12 Actions (Type . to access issue actions) 헤더

--- front-sk/src/views/Main.vue
+++ front-sk/src/views/Main.vue
@@ -17,7 +17,7 @@
        <v-btn icon>
         <v-icon>{{icons.AccountCircle}}</v-icon>
       </v-btn>
-
+<!-- 로그인/로그아웃/signup 표시 -->
       <v-btn icon>
         <v-icon>{{icons.Magnify}}</v-icon>
       </v-btn>
@@ -27,16 +27,16 @@
       <v-row
         align="start"
         no-gutters
-        style="height: 150px;"
+        style="height: 130px;"
         class="mainSearch ">
         <v-col>
           
         </v-col>
         <v-col>
-             <div class="search">
-                <input type="text" class="searchTerm" placeholder="What are you looking for?">
+             <div class="search" >
+                <input type="text" class="searchTerm" placeholder="What are you looking for?" >
                 <button type="submit" class="searchButton">
-                    <i class="fa fa-search"></i>
+                    
                 </button>
             </div>
         </v-col>
@@ -51,16 +51,28 @@
         <v-row no-gutters>
           <template v-for="(store, index) in Stores">
             <v-col :key="index">
-              <v-card
-                :loading="loading"
-                class="mx-auto my-12 storeRank"
-        
-                max-width="374"
-              >
-                <v-img
-                  height="250"
-                  :src="store.picture"
-                ></v-img>
+              <v-hover v-slot:default="{ hover }">
+                <v-card
+                  :loading="loading"
+                  class="mx-auto my-12 storeRank"
+                  max-width="374">
+                
+                  <v-img
+                  height="200"
+                  :src="store.picture">
+                
+                  <v-expand-transition>
+                  <div
+                    v-if="hover"
+                    class="d-flex transition-fast-in-fast-out v-card--reveal white--text"
+                    style="height: 100%;"
+                    >
+                식당 상세 정보
+              </div>
+              </v-expand-transition>
+                
+                
+                </v-img>
 
                 <v-card-title>{{store.name}}</v-card-title>
 
@@ -84,33 +96,15 @@
                   <div class="my-4 subtitle-1 black--text">
                     {{store.address}}
                   </div>
-
-  
-                </v-card-text>
-
-                <v-divider class="mx-4"></v-divider>
-
-                <v-card-title>HashTag</v-card-title>
-
-                <v-card-text>
-                  <v-chip-group
+               <v-chip-group
                    column
                   >
                     <v-chip v-for="(h, i) in store.hashtag" :key="i" >#{{h}}</v-chip>
-
-                  </v-chip-group>
+                   </v-chip-group>
                 </v-card-text>
 
-                <v-card-actions>
-                  <v-btn
-                    color="deep-purple accent-4"
-                    text
-                    @click="reserve"
-                  >
-                    Detail
-                  </v-btn>
-                </v-card-actions>
               </v-card>
+              </v-hover>
                 </v-col>
                 <v-responsive
                   v-if="n === 2"
@@ -122,13 +116,35 @@
       </v-col>
          
         <v-col cols="3">
-            <v-card
-            class="pa-2"
-            outlined
-            tile
-          >
-            One of three columns
-          </v-card>
+             <v-row align="center">
+                <v-card
+                  class="mx-auto"
+                  max-width="400"
+                  tile
+                >
+                  <v-list
+                    flat=true
+                    avatar=true
+                    rounded=true
+                  >
+                    <v-subheader><v-icon>{{icons.Crown}}</v-icon>이달의 리뷰왕<v-icon>{{icons.Crown}}</v-icon></v-subheader>
+                    <v-list-item-group v-model="item" color="primary">
+                      <v-list-item
+                        v-for="(item, i) in items"
+                        :key="i"
+                      >
+                        <v-list-item-avatar v-if="item.avatar">
+                          <v-img :src="item.avatar"></v-img>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                          <v-list-item-title>  {{i+1}}등: {{item.title}}</v-list-item-title>
+                          <v-list-item-subtitle></v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </v-card>
+              </v-row>
         </v-col>
   
     </v-row>
@@ -141,7 +157,7 @@
 
 <script>
 
-import { mdiMagnify, mdiAccountCircle,mdiForum, mdiPasta } from '@mdi/js'
+import { mdiMagnify, mdiAccountCircle,mdiForum, mdiCrown } from '@mdi/js'
 import {mapState} from 'vuex'
 
 export default {
@@ -151,10 +167,37 @@ export default {
                 Magnify: mdiMagnify,
                 AccountCircle: mdiAccountCircle,
                 Forum: mdiForum,
-                Pasta: mdiPasta
+                Crown: mdiCrown
             },
             loading: false,
             selection: 1,
+                items: [
+                          {
+                            avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                            title: 'alex',
+                            subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+                          },
+                          {
+                            avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                            title: 'emma',
+                            subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+                          },
+                          {
+                            avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                            title: 'harry',
+                            subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+                          },
+                          {
+                            avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                            title: 'lebecca',
+                            subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+                          },
+                          {
+                            avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                            title: 'ron',
+                            subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+                          },
+                        ],
 
 
         }
@@ -182,7 +225,7 @@ export default {
 .mainSearch {
     padding-top: 90px;
     height: 250px !important; 
-    background-color: yellow;
+    background-color: orange;
 }
 .search {
   width: 100%;
@@ -192,7 +235,7 @@ export default {
 
 .searchTerm {
   width: 100%;
-  border: 3px solid black;
+  border: 3px solid orangered;
   border-right: none;
   padding: 5px;
   height: 36px;
@@ -203,14 +246,14 @@ export default {
 }
 
 .searchTerm:focus{
-  color: black;
+  color: orangered;
 }
 
 .searchButton {
   width: 40px;
   height: 36px;
-  border: 1px solid black;
-  background: black;
+  border: 1px solid orangered;
+  background: orangered;
   text-align: center;
   color: #fff;
   border-radius: 0 5px 5px 0;
@@ -219,6 +262,21 @@ export default {
 }
 .storeRank {
   margin-bottom: 30px;
+}
+.v-card__title{
+  line-height: 0.5rem;
+}
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .5;
+  position: absolute;
+  width: 100%;
+  background-color: orange;
+  left: 0px;
+  color: white;
+  text-align: center;
 }
 
 </style>
