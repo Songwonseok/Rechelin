99a22c9fcc6872487364329cf13faac99efc2126
ichhjkim <nillcafri123@gmail.com>
2020-02-07 11:16:27 +0900

    git....git...

--- front-sk/src/main.js
+++ front-sk/src/main.js
@@ -23,6 +23,7 @@ import "./vee-validate";
 import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
 import awsconfig from './aws-exports'
+
 import VueLoading from 'vuejs-loading-plugin'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 Amplify.configure(awsconfig)
