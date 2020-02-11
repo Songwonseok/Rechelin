ea3f432d96e2c167315ab3acb72125228c7c8ca5
spagettiUser <tqbs1024@gmail.com>
2020-02-11 12:42:02 +0900

    router fix

--- front-sk/src/views/Main.vue
+++ front-sk/src/views/Main.vue
@@ -216,7 +216,10 @@
         console.log(t);
         if(t===0){
            //유저 페이지 
-           this.$router.push({ path: "sideMenuUser" });
+           if(sessionStorage.getItem("userEmail"))
+              this.$router.push({ path : "sideMenuUserPage"})
+           else
+              this.$router.push({ path: "sideMenuLogin" });
         }else if(t==1){
            //음식점 검색
             this.$router.push({ path: "sideMenuFood" });
