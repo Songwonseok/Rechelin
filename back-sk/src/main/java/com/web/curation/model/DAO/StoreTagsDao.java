da0e15af50e2a6def88ec3f7c163e167f3a6598f
juyeon <wjg365@naver.com>
2020-02-11 10:21:21 +0900

    querydsl 이름 수정

--- back-sk/src/main/java/com/web/curation/model/DAO/StoreTagsDao.java
+++ back-sk/src/main/java/com/web/curation/model/DAO/StoreTagsDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.curation.model.DTO.Hashtag;
 import com.web.curation.model.DTO.Storetags;
-import com.web.curation.model.QueryDSL.CustomRepository;
+import com.web.curation.model.querydsl.CustomRepository;
 
 public interface StoreTagsDao extends JpaRepository<Storetags, Long>,CustomRepository{
 	List<Storetags> findAllByHashtag(Hashtag hashtag);
