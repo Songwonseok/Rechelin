8ef544d1fe491f46a0b7d1ac9b33198a16a964ca
송원석 <dnjstjr0517@gmail.com>
2020-02-18 14:23:52 +0900

    merge

--- back-sk/src/main/java/com/web/curation/WebCurationApplication.java
+++ back-sk/src/main/java/com/web/curation/WebCurationApplication.java
@@ -3,7 +3,9 @@ package com.web.curation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @EnableJpaAuditing
 @SpringBootApplication
 public class WebCurationApplication {
