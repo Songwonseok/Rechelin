da572f2fa1e2e43b9f75077a968526d8b107a761
lllilllilllilili <hik3562@hufs.ac.kr>
2020-03-03 11:34:47 +0900

    docs: ✏️ back-sk remove

--- back-sk/src/main/java/com/web/curation/service/JwtService.java
+++
@@ -1,9 +0,0 @@
-package com.web.curation.service;
-
-import com.web.curation.model.DTO.User;
-
-public interface JwtService {
-	String makeJwt(User user);
-
-	int checkJwt(String jwt) throws Exception;
-}
